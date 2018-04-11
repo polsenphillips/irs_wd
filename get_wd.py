36da9f084c63de10b0539a3234d5861f8152d8fa
polsenphillips <polsenphillips@gmail.com>
2018-04-10 20:07:27 -0400

    fixed requests issue

--- get_wd.py
+++ get_wd.py
@@ -1,29 +1,65 @@
+
+
+import datetime
 import pandas as pd
-import pprint
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 import requests
-import grequests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
 
-url_list = []
-last_current_url = "https://apps.irs.gov/app/picklist/list/writtenDeterminations.html?indexOfFirstRow=61400&sortColumn=number&value=&criteria=&resultsPerPage=200&isDescending=true"
-url_1 = "https://apps.irs.gov/app/picklist/list/writtenDeterminations.html?resultsPerPage=200&sortColumn=number&indexOfFirstRow=0&criteria=&value=&isDescending=true"
-url_1_req = requests.get(url_1)
 
-soup = BeautifulSoup(url_1_req.content, 'html.parser')
-target_info = soup.findAll(True, {'class':['odd', 'even']})
 
-table = []
+def generate_urls():
+    urls = []
+    url = "https://apps.irs.gov/app/picklist/list/writtenDeterminations.html?resultsPerPage=200&sortColumn=number&indexOfFirstRow=0&criteria=&value=&isDescending=true"
+    r = requests.get(url)
+    iterator = 0
+    while r.status_code == 200:
+        urls.append(url)
+        iterator += 200
+        url = "https://apps.irs.gov/app/picklist/list/writtenDeterminations.html?resultsPerPage=200&sortColumn=number&indexOfFirstRow="+ str(iterator) + "&criteria=&value=&isDescending=true"
+        r = requests.get(url)    
+    print("URLS generated.")    
+    return urls
 
-for row in target_info:
-    values = row.findAll("td")
-    url = values[0].find("a").get("href")
-    number = values[0].find("a").getText(strip = True)
-    uilc = values[1].getText(strip = True)
-    subject = values[2].getText(strip = True).replace("&quot;", "'")
-    release_date = values[3].getText(strip = True)
-    table.append([url,number, uilc, subject, release_date])
+field_names = {}
+
+def parse_soup(urls):##Takes a really long time. Maybe use "lxml" argument?
+    table = []
+    for url in urls:
+        url_req = requests.get(url)
+        soup = BeautifulSoup(url_req.content, 'html.parser')
+        target_info = soup.findAll(True, {'class':['odd', 'even']})
+        for row in target_info:
+            values = row.findAll("td")
+            url = values[0].find("a").get("href")
+            number = values[0].find("a").getText(strip = True)
+            uilc = values[1].getText(strip = True)
+            subject = values[2].getText(strip = True).replace("&quot;", "'")
+            release_date = values[3].getText(strip = True)
+            table.append([url, number, uilc, subject, release_date])  
+    print("Table populated.")    
+    return table
+
+
+df = pd.DataFrame(data = table)
+
+def add_header():
+    df.columns = ["url", "number", "uilc", "subject", "release_date"]
+    return df  
+
+def main():
+    parse_soup(generate_urls())
     
+    add_header()
+    df.to_csv("written_determinations_{0}.csv".format(str(datetime.date.today())))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
 
